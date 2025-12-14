cab92e8d0db5bcc919214f75b04215f324f44c89
Ana Rosa Marmolejo Cervantes <anarmc94@gmail.com>
2025-12-14 14:02:48 -0600

    feat: update Supabase dependencies and implement client creation for server and middleware

---
+++ src/utils/supabase/client.ts
@@ -0,0 +1,11 @@
+
+import { createBrowserClient } from "@supabase/auth-helpers-nextjs";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const createClient = () =>
+  createBrowserClient(
+    supabaseUrl!,
+    supabaseKey!,
+  );
